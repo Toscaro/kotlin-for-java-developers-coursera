a79dc4dba0c669246c4d4bce457b0d34acea01f3
Toscaro <lucas@toscaro.com.br>
2021-05-18 16:11:38 -0300

    [Week2] Using var and val variables in Kotlin

---
+++ src/main/kotlin/basics/VariablesClass.kt
@@ -0,0 +1,17 @@
+package basics
+
+fun main() {
+    //It is possible to reassign a val variable type.
+    var mutableVariable = 10
+    println(mutableVariable)
+    mutableVariable = 5
+    println(mutableVariable)
+
+    //readOnlyVariable = "Hi!"
+    //Impossible to reassign a val variable type, because val is a read-only variable.
+    val readOnlyVariable = "Hello!"
+    println(readOnlyVariable)
+
+    //Also, it's not possible to change the type of var type, it will cause a Compilation error
+    //mutableVariable = "Test!"
+}
