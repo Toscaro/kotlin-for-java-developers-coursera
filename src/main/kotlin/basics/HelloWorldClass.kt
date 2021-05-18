f4cfe2ca4ccd6f70d48a06e1b7e1e2ad1c1d542d
Toscaro <lucas@toscaro.com.br>
2021-05-18 16:04:03 -0300

    [Week2] Handle some strings examples with Kotlin

--- src/main/kotlin/basics/HelloWorldClass.kt
+++ src/main/kotlin/basics/HelloWorldClass.kt
@@ -1,6 +1,21 @@
 package basics
 
-fun main() {
+fun main(args: Array<String>) {
     val name = "Kotlin"
     println("Hello, $name!")
+
+    //Ternary does not exists in Kotlin
+    val name2 = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello, $name2!")
+
+    //Print null ins case of index is 0
+    println("Hello, ${args.getOrNull(0)}!")
+
+    //It call the foo() function twice, there is no cache or something like that when using with strings.
+    println("First ${foo()}, second ${foo()}")
+}
+
+fun foo(): String {
+    println("Calculating foo()")
+    return "foo"
 }
