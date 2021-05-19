6076231f23cbac136547273a2e81e24bcc1bc91e
Toscaro <lucas@toscaro.com.br>
2021-05-19 14:25:31 -0300

    [Week2] [Test] [Solved] Sum as an extension function

--- src/main/kotlin/exercises/week2/SumAsAnExtensionFunction.kt
+++ src/main/kotlin/exercises/week2/SumAsAnExtensionFunction.kt
@@ -5,15 +5,15 @@ package exercises.week2
  *
  * Change the 'sum' function so that it was declared as an extension to List<Int>.
  */
-fun sum(list: List<Int>): Int {
+fun List<Int>.sum(): Int {
     var result = 0
-    for (i in list) {
+    for (i in this) {
         result += i
     }
     return result
 }
 
 fun main(args: Array<String>) {
-    val sum = sum(listOf(1, 2, 3))
+    val sum = listOf(1, 2, 3).sum()
     println(sum)    // 6
 }
