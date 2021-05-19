d3bda83da305986b71bbe80ccf31962c1d8e57e3
Toscaro <lucas@toscaro.com.br>
2021-05-19 12:24:25 -0300

    [Week2] Solving the Checking Identifier exercise

---
+++ src/main/kotlin/exercises/week2/CheckIdentifier.kt
@@ -0,0 +1,32 @@
+package exercises.week2
+
+/**
+ * Implement the function that checks whether a string is a valid identifier.
+ * A valid identifier is a non-empty string that starts with a letter or underscore and consists of only letters,
+ * digits and underscores.
+ */
+fun isValidIdentifier(s: String): Boolean {
+    return s.isNotEmpty() && startsWithValidChar(s[0]) && notContainAInvalidChar(s)
+}
+
+fun startsWithValidChar(c: Char): Boolean {
+    return (c.isLetter() || c in "_")
+}
+
+fun notContainAInvalidChar(string: String): Boolean {
+    for (c: Char in string) {
+        if (c !in "_" && !c.isLetterOrDigit()) {
+            return false
+        }
+    }
+    return true
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
