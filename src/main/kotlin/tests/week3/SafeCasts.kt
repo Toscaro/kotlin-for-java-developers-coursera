5a1a275b605404364f369791a0438941a81308c0
Toscaro <lucas@toscaro.com.br>
2021-05-20 17:28:31 -0300

    [Week3] [Test] [Solved] Completing Safe Casts test

--- src/main/kotlin/tests/week3/SafeCasts.kt
+++ src/main/kotlin/tests/week3/SafeCasts.kt
@@ -8,7 +8,10 @@ package tests.week3
  * Declare the s variable to make the first line print null and the second one throw an exception.
  */
 fun main(args: Array<String>) {
-    val s = TODO()
+    val s = "Hi!"
+
+    // Prints null because the s is a string and as is using ? to return null.
     println(s as? Int)    // null
+    // Wrong cast, as will cause class cast, String cannot be casted to Int
     println(s as Int?)    // exception
 }
